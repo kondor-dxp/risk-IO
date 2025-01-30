25c2a28a4c94d13694777a140deb8facc2dcebb1
KND-ULTRABOOK\dario <dariomunao@gmail.com>
2025-01-30 19:47:25 +0100

    Components created

---
+++ src/components/Elements.js
@@ -0,0 +1,10 @@
+const Elements = () => {
+    return (
+        <>
+        <a href="#" id="scroll-top" className="scroll-top d-flex align-items-center justify-content-center"><i className="bi bi-arrow-up-short" /></a>
+        <div id="preloader" />
+      </>
+    )
+}
+
+export default Elements
--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -6,21 +6,21 @@ const ServicesSection = () => {
                 <div className="col-lg-4 d-flex" data-aos="fade-up" data-aos-delay={100}>
                 <div className="service-item position-relative">
                     <img src="img/service1.gif" className="service-icon" />
-                    <h4><a href className="stretched-link">Effective Risk Evaluation</a></h4>
+                    <h4><a href="#" className="stretched-link">Effective Risk Evaluation</a></h4>
                     <p>Empowering you to identify, assess, and management risks with precision and confidence</p>
                 </div>
                 </div>{/* End Service Item */}
                 <div className="col-lg-4 d-flex" data-aos="fade-up" data-aos-delay={200}>
                 <div className="service-item position-relative">
                     <img src="img/service2.gif" className="service-icon" />
-                    <h4><a href className="stretched-link">Continual Monitoring</a></h4>
+                    <h4><a href="#" className="stretched-link">Continual Monitoring</a></h4>
                     <p>Effortlessly analyze and evaluate risks using our comprehensive risk assessment software</p>
                 </div>
                 </div>{/* End Service Item */}
                 <div className="col-lg-4 d-flex" data-aos="fade-up" data-aos-delay={300}>
                 <div className="service-item position-relative">
                     <img src="img/service3.gif" className="service-icon" />
-                    <h4><a href className="stretched-link">Threat Mitigation</a></h4>
+                    <h4><a href="#" className="stretched-link">Threat Mitigation</a></h4>
                     <p>Create effective mitigation strategies to minimize the impact of identified risks</p>
                 </div>
                 </div>{/* End Service Item */}
