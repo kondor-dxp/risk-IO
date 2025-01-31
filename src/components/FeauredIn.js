838924c4e1d76f658d7e040373079c035ce997b4
Dario <dariomunao@gmail.com>
2025-01-31 02:49:00 +0100

    Section featured in created

---
+++ src/components/FeauredIn.js
@@ -0,0 +1,30 @@
+const FeaturedIn = () => {
+    return (
+        <section id="call-to-action" className="call-to-action section accent-background">
+            <div className="container">
+                <div className="row justify-content-center" data-aos="zoom-in" data-aos-delay={100}>
+                    <div className="col-xl-10">
+                        <div className="text-center">
+                            <h3>AS FEATURED IN: </h3>
+<p>
+
+    <br></br>
+</p>
+                            
+                            
+                            <a className="cta-btn" href="https://www.linkedin.com/showcase/risk-io/posts/?feedView=all" target="_blank">
+                            LinkedIN
+                            </a>
+                            
+                            <a className="cta-btn" href="https://www.securitysa.com/22878r" target="_blank">
+                            Smart Security Solutions
+                            </a>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default FeaturedIn
