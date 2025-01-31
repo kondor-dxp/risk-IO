de53afa8225896d08f4be1773d3564383f4c69f1
Dario <dariomunao@gmail.com>
2025-01-31 02:50:17 +0100

    All new sections added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import Footer from './components/Footer';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import HeroSection from './components/HeroSection';
 import ServicesSection from './components/ServicesSection';
-import Elements from './components/Elements';
 import AboutSection from './components/AboutSection';
+import ContactSection from './components/ContactSection';
+import Elements from './components/Elements';
 import UseScript from './components/UseScript';
+import FeaturesSection from './components/FeaturesSection';
+import AtWorkSection from './components/AtWorkSection';
+import FeaturedIn from './components/FeauredIn';
 
 function App() {
   return (
@@ -15,20 +19,24 @@ function App() {
         <HeroSection></HeroSection>
         <ServicesSection></ServicesSection>
         <AboutSection></AboutSection>
-
-        <Elements></Elements>
-        
-        <UseScript src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></UseScript>
-        <UseScript src="assets/vendor/php-email-form/validate.js"></UseScript>
-        <UseScript src="assets/vendor/aos/aos.js"></UseScript>
-        <UseScript src="assets/vendor/glightbox/js/glightbox.min.js"></UseScript>
-        <UseScript src="assets/vendor/purecounter/purecounter_vanilla.js"></UseScript>
-        <UseScript src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></UseScript>
-        <UseScript src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></UseScript>
-        <UseScript src="assets/vendor/swiper/swiper-bundle.min.js"></UseScript>
-        <UseScript src="assets/js/main.js"></UseScript>
+        <FeaturesSection></FeaturesSection>
+        <AtWorkSection></AtWorkSection>
+        <FeaturedIn></FeaturedIn>
+        <ContactSection></ContactSection>
       </main>
       <Footer></Footer>
+
+      <Elements></Elements>
+        
+      <UseScript src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></UseScript>
+      <UseScript src="assets/vendor/php-email-form/validate.js"></UseScript>
+      <UseScript src="assets/vendor/aos/aos.js"></UseScript>
+      <UseScript src="assets/vendor/glightbox/js/glightbox.min.js"></UseScript>
+      <UseScript src="assets/vendor/purecounter/purecounter_vanilla.js"></UseScript>
+      <UseScript src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></UseScript>
+      <UseScript src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></UseScript>
+      <UseScript src="assets/vendor/swiper/swiper-bundle.min.js"></UseScript>
+      <UseScript src="assets/js/main.js"></UseScript>
     </div>
   );
 }
